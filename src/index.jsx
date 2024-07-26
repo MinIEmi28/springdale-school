b989b26f929110ddc1fc27c91571cc8a9cd9aaec
Priya Tiwari <priyatiwari211217@gmail.com>
2024-07-26 12:02:51 +0530

    updating index.jsx for appropreate createRoot()

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css'; // Add your global styles here
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import './index.css'; // Add your global styles here
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>,
+//     document.getElementById('root')
+// );
+
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client'
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));   
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>   
+
 );
