f91fb3d5767f9f56032e4d6a2213bb92f49b5129
Priya Tiwari <priyatiwari211217@gmail.com>
2024-07-27 06:29:44 +0530

    changing index.jsx into index.js

--- src/index.js
+++ src/index.js
@@ -10,31 +10,31 @@
 //     document.getElementById('root')
 // );
 
-import React from 'react';
-import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client'
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));   
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>   
-
-);
-
 // import React from 'react';
 // import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client'
 // import App from './App';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));   
 
-
 // root.render(
-//   React.createElement(
-//     React.StrictMode,
-//     null,
-//     React.createElement(App,   
-//  null)
-//   )
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>   
+
 // );
+
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client'
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));   
+
+
+root.render(
+  React.createElement(
+    React.StrictMode,
+    null,
+    React.createElement(App,   
+ null)
+  )
+);
